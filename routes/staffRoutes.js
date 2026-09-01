9b39be5178b281ed1935453b93f9a09270f7a1e5
Hamza <32330677@students.liu.edu.lb>
2026-09-01 07:05:16 +0300

    1/9 ver2

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
 const staffController = require('../controllers/staffController');
 const staffLifecycleController = require('../controllers/staffLifecycleController');
 const staffAssignmentController = require('../controllers/staffAssignmentController');
